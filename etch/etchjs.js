90a74ec3ebf5a294a9e2c92e5353c26b39168859
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-18 16:16:29 -0600

    working on etch

--- etch/etchjs.js
+++ etch/etchjs.js
@@ -1,1 +1,14 @@
 //ETCH A SKETCH JAVASCRIPT CODE//
+const container = document.getElementById("container");
+let sqsize = 8;
+
+console.log(sqsize);
+
+for (let i = 0; i < sqsize; i++) {
+    const divx = document.createElement("div");
+    container.appendChild(divx);
+}
+
+
+
+
