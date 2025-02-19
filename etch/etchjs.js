a0dba519a652c2fe42dd0ddb1c2bb2ee8e126ee1
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-19 12:54:37 -0600

    finally got the grid

--- etch/etchjs.js
+++ etch/etchjs.js
@@ -1,21 +1,31 @@
 //ETCH A SKETCH JAVASCRIPT CODE//
 
-const container = document.getElementById("container"); //container for boxes
 
-let gridSize = 16; //grid size variable to be edited by user
+let gridSize = 11; //grid size variable to be edited by user
 let boxes = gridSize*gridSize; //square to create appropriate div count
 
 
-let cWidth = container.clientWidth //get container width in px 
-let boxWidth = cWidth/gridSize  //calc box width
-let boxPx = boxWidth + "px"; //convert box width to px
+function fillContainer() {
+    const container = document.getElementById("container"); //container for boxes
+    container.innerHTML = ''; //clear container on resize
+    let cWidth = container.clientWidth - 4 //get container width in px 
+    let boxWidth = cWidth/gridSize  //calc box width
+    let boxPx = boxWidth + "px"; //convert box width to px
 
-// GENERATE THE NUMBER OF BOXES BASED ON SIZE //
-for (i = 0; i < boxes; i++) {
-    const divx = document.createElement("div");
-    divx.className = "divx";
-    divx.style.width = boxPx; 
-    divx.style.height = boxPx; 
-    container.appendChild(divx);
+    // GENERATE THE NUMBER OF BOXES BASED ON SIZE //
+    for (i = 0; i < boxes; i++) {
+        const divx = document.createElement("div");
+        divx.className = "divx";
+        divx.style.width = boxPx; 
+        divx.style.height = boxPx; 
+        container.appendChild(divx);
+    }
 }
 
+fillContainer();
+
+
+// window.addEventListener("resize", (e) => {
+//     console.log("window resized");
+//     fillContainer();
+// });
