31164e20ab7320cab179a19a7290bbfbdc3bb898
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-19 09:10:37 -0600

    don't think I did anything useful

--- etch/etchjs.js
+++ etch/etchjs.js
@@ -1,17 +1,21 @@
 //ETCH A SKETCH JAVASCRIPT CODE//
 
 const container = document.getElementById("container"); //container for boxes
-let size = 11; //variable to be edited by user
-let boxes = size*size; //square to create appropriate div count
-let cWidth = container.clientWidth; //get container width in px
-let boxWidth = cWidth/size + "px"; //calc box width in px
 
+let gridSize = 16; //grid size variable to be edited by user
+let boxes = gridSize*gridSize; //square to create appropriate div count
+
+
+let cWidth = container.clientWidth //get container width in px 
+let boxWidth = cWidth/gridSize  //calc box width
+let boxPx = boxWidth + "px"; //convert box width to px
 
 // GENERATE THE NUMBER OF BOXES BASED ON SIZE //
 for (i = 0; i < boxes; i++) {
     const divx = document.createElement("div");
     divx.className = "divx";
-    divx.style.width = boxWidth; //height set in CSS with aspect-ratio: 1/1;
+    divx.style.width = boxPx; 
+    divx.style.height = boxPx; 
     container.appendChild(divx);
 }
 
