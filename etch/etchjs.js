5b7b91d8699529c912c430445a2bc5c5334943d4
clbandy45 <187895362+clbandy45@users.noreply.github.com>
2025-02-18 21:32:42 -0600

    finally got the boxes

--- etch/etchjs.js
+++ etch/etchjs.js
@@ -1,14 +1,17 @@
 //ETCH A SKETCH JAVASCRIPT CODE//
-const container = document.getElementById("container");
-let sqsize = 8;
 
-console.log(sqsize);
+const container = document.getElementById("container"); //container for boxes
+let size = 11; //variable to be edited by user
+let boxes = size*size; //square to create appropriate div count
+let cWidth = container.clientWidth; //get container width in px
+let boxWidth = cWidth/size + "px"; //calc box width in px
 
-for (let i = 0; i < sqsize; i++) {
+
+// GENERATE THE NUMBER OF BOXES BASED ON SIZE //
+for (i = 0; i < boxes; i++) {
     const divx = document.createElement("div");
+    divx.className = "divx";
+    divx.style.width = boxWidth; //height set in CSS with aspect-ratio: 1/1;
     container.appendChild(divx);
 }
-
-
-
 
